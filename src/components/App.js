cedee570a5ec1a5cd5b13a2c5e54c6ed762a3692
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-04 00:23:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,48 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react';
+import UseMemo from './UseMemo';
+import ReactMemo from './ReactMemo';
 
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [customTask, setCustomTask] = useState('');
+
+  const addTask = () => {
+    setTasks((prevTasks) => [...prevTasks, 'New todo']);
+  };
+
+  const incrementCounter = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const handleCustomTaskChange = (e) => {
+    setCustomTask(e.target.value);
+  };
+
+  const handleCustomTaskSubmit = () => {
+    if (customTask.length > 5) {
+      setTasks((prevTasks) => [...prevTasks, customTask]);
+      setCustomTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <button onClick={addTask}>Add Todo</button>
+      <button onClick={incrementCounter}>Increment Counter</button>
+      <div>
+        <input
+          type="text"
+          value={customTask}
+          onChange={handleCustomTaskChange}
+        />
+        <button onClick={handleCustomTaskSubmit}>Submit</button>
+      </div>
+      <UseMemo tasks={tasks} />
+      <ReactMemo counter={counter} />
+    </div>
+  );
+};
+
+export default App;
