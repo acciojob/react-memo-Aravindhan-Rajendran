dc0746019524577330b7fbd2da42ec1f1a0343f4
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-12 23:58:33 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,10 +1,21 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 const UseMemo = ({ tasks }) => {
+  const [count, setCount] = useState(0);
+  
+  // Memoizing the tasks to avoid unnecessary re-renders
   const memoizedTasks = useMemo(() => tasks, [tasks]);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
   return (
     <div>
+      <p>
+        Count: {count}
+        <button id="calc" onClick={handleIncrement}>+</button>
+      </p>
       <h2>Tasks List</h2>
       <ul id="task-list">
         {memoizedTasks.map((task, index) => (
