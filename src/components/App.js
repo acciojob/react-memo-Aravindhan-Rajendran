bcc1c42cc605b256bb41c9e0117b2925b2ef3f7e
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-12 23:31:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ const App = () => {
     <div id="main">
       <h1>Task Manager</h1>
       <button id="add-todo-btn" onClick={addTask}>Add Todo</button>
-      <button id="incr-cnt" onClick={incrementCounter}>Increment Counter</button>
+      <button id="incr-cnt" onClick={incrementCounter}>Increment Counter: {counter}</button>
       <div>
         <input
           id="skill-input"
