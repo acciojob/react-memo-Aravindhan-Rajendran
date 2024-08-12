467747b2ba96e161e3317753b699b4c57312bc78
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-12 22:20:11 +0530

    Update ReactMemo.js

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ReactMemo = React.memo(({ counter }) => {
   console.log('ReactMemo re-rendered');
-  
+
   return (
     <div>
-      <h2>Counter: {counter}</h2>
+      <h2>Counter: {counter}</h2> {/* Ensure this displays the counter correctly */}
     </div>
   );
 });
